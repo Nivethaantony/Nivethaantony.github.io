9f5b542ed703026e9e10e6184c8432763644f18e
Nivetha <nivethaantony98@gmail.com>
2020-10-21 08:41:45 +0200

    Add concat method in js file

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
-/*var x=10;
-var y=20;
-var z;
-z=x;
-x=y;
-y=z;
-console.log(x,y);*/
+ var x=[12,23,45,22,11];
+ var y=[2,5,7,9,25];
+ var z=x.concat(y);
+ console.log(z);
 /*var x=[1,5,8,3,7];
 x.push(20);
-console.log(x);*/
+console.log(x);
 var x=[1,2,3,1,4,1];
 x.findIndex(1);
-console.log(x);
+console.log(x);*/
