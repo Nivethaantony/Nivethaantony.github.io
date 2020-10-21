8d8ebb93c53781e6bea055df094c84eeaf8c36cc
Nivetha <nivethaantony98@gmail.com>
2020-10-21 08:55:04 +0200

    Add join method in js file

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
- /*     1   var x=[12,23,45,22,11];
+ /*     1)   var x=[12,23,45,22,11];
  var y=[2,5,7,9,25];
  var z=x.concat(y);
  console.log(z);*/
-var x=[1,5,8,3,7];
+
+/*       2 )  var x=[1,5,8,3,7];
 var y,z;
 z=x.indexOf(5);
 y=x.indexOf(5,2);
 console.log(y);
-console.log(z);
+console.log(z);*/
+
+var x=[1,5,3,7,4];
+var y=x.join();
+var z=x.join("*");
+var a=x.join("@");
+console.log(y,z,a);
