892d4a8a02be519b254de5901df856a0bf836252
Nivetha <nivethaantony98@gmail.com>
2020-10-21 08:48:32 +0200

    Add indexOf method in js file

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
- var x=[12,23,45,22,11];
+ /*     1   var x=[12,23,45,22,11];
  var y=[2,5,7,9,25];
  var z=x.concat(y);
- console.log(z);
-/*var x=[1,5,8,3,7];
-x.push(20);
-console.log(x);
-var x=[1,2,3,1,4,1];
-x.findIndex(1);
-console.log(x);*/
+ console.log(z);*/
+var x=[1,5,8,3,7];
+var y,z;
+z=x.indexOf(5);
+y=x.indexOf(5,2);
+console.log(y);
+console.log(z);
