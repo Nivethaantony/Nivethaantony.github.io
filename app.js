2b9dc488854ea6956f38509eca470cbdc5d6acd8
Nivetha <nivethaantony98@gmail.com>
2020-10-21 09:03:12 +0200

    Add lastIndexOf method in js file

--- app.js
+++ app.js
@@ -10,8 +10,14 @@ y=x.indexOf(5,2);
 console.log(y);
 console.log(z);*/
 
-var x=[1,5,3,7,4];
+/*       3)     var x=[1,5,3,7,4];
 var y=x.join();
 var z=x.join("*");
 var a=x.join("@");
+console.log(y,z,a);*/
+var x=[4,2,5,1,4,1,3];
+var y,z,a;
+y=x.lastIndexOf(1);
+z=x.lastIndexOf(1,4);
+a=x.lastIndexOf(1,2);
 console.log(y,z,a);
